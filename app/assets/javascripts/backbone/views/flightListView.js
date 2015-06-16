b3894dba38209030aad21c4203ab7997e13caa5c
Drew Showalter <maelstroswim@gmail.com>
2015-06-16 19:06:19 +1000

    fixed views and listview for flights

--- app/assets/javascripts/backbone/views/flightListView.js
+++ app/assets/javascripts/backbone/views/flightListView.js
@@ -3,12 +3,14 @@ var app = app || {};
 app.FlightListView = Backbone.View.extend({
   el: '#view',
 
-  render: function() {
+  render: function () {
     var flightListViewTemplate = $('#flightListViewTemplate').html();
     this.$el.html(flightListViewTemplate);
 
+    var view = this;
     app.flights.each(function(flight) {
-      app.flightView = new app.FlightView({model: flight});
+      app.flightView = new app.FlightView({model: flight, parentView: view.$el});
+      app.flightView.render( view.$el );
       // app.flightView.render();
     });
   },
