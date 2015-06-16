c654459f86724bc077ca957ef662679f4e03a8ca
Toby Flemming <tobyf93@hotmail.com>
2015-06-16 13:10:51 +1000

    Change Gemfile.  Create seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Flight.destroy_all
+Plane.destroy_all
+Reservation.destroy_all
+User.destroy_all
+
+# Flights
+f1 = Flight.create :origin => 'Sydney', :destination => 'Kazakhstan', :date => '2015-06-20'
+f2 = Flight.create :origin => 'Kazakhstan', :destination => 'Sydney', :date => '2015-06-20'
+f3 = Flight.create :origin => 'Sydney', :destination => 'Brisbane', :date => '2015-06-20'
+
+# Planes
+p1 = Plane.create :rows => 10, :columns => 10, :name => 'Big Bitch Airlines'
+p1.flights << f1 << f2 << f3
