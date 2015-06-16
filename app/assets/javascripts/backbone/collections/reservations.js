991836a59475ef354440d5f009e1740c4c66fd2a
Drew Showalter <maelstroswim@gmail.com>
2015-06-17 09:27:14 +1000

    morning merge

--- app/assets/javascripts/backbone/collections/reservations.js
+++ app/assets/javascripts/backbone/collections/reservations.js
@@ -3,6 +3,7 @@ var app = app || {};
 app.Reservations = Backbone.Collection.extend({
   model: app.Reservation,
 
+  // Obviously won't stay like this
   url: function () {
     console.log(this.models[0].get('plane_id'));
     return '/planes/' + this.models[0].get('plane_id') + '/flights/' + this.models[0].get('flight_id') + '/seats'
