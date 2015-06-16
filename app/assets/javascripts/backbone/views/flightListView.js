d763c50d31f14c059db1ffb6ef25e0c18f596b28
Toby Flemming <tobyf93@hotmail.com>
2015-06-16 16:58:58 +1000

    Flights now linked to Planes

--- app/assets/javascripts/backbone/views/flightListView.js
+++ app/assets/javascripts/backbone/views/flightListView.js
@@ -1,11 +1,9 @@
 var app = app || {};
 
 app.FlightListView = Backbone.View.extend({
-
   el: '#view',
 
   render: function() {
-    console.log('rendering the flight list view')
     var flightListViewTemplate = $('#flightListViewTemplate').html();
     this.$el.html(flightListViewTemplate);
 
