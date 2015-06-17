1ed74598cc381e0523327ae8c29f12cdee4c8948
Toby Flemming <tobyf93@hotmail.com>
2015-06-17 11:10:53 +1000

    Poller working with stubs

--- app/assets/javascripts/backbone/utilities/poller.js
+++ app/assets/javascripts/backbone/utilities/poller.js
@@ -4,7 +4,6 @@ app.Poller = function(options) {
   for (var option in options) {
     this[option] = options[option];
   }
-
   // Check for valid object attributes
   if (typeof this.collection === 'undefined') {
     console.warn('Must specify a collection to poll.');
@@ -12,9 +11,23 @@ app.Poller = function(options) {
   if (typeof this.seconds === 'undefined') {
     console.warn('Must specify a time (in seconds) to poll.');
   }
+
+  app.currentPoller = this;
 };
 
 app.Poller.prototype.start = function() {
-  console.log('starting to poll!');
+  console.log('POLL: Starting...');
+  app.currentPoller.fetch();
 };
 
+app.Poller.prototype.fetch = function() {
+  console.log('POLL: Fetching data...');
+  app.currentPoller.onSuccess();
+};
+
+app.Poller.prototype.onSuccess = function() {
+  console.log('POLL: Fetched!  (next fetch in ' + app.currentPoller.seconds + ' seconds)');
+
+  console.log(app.currentPoller.collection);
+  setTimeout(app.currentPoller.fetch, app.currentPoller.seconds * 1000);
+};
